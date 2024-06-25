24ed8a10b328de84065b7f37cd27e06404284954
MateuszGacek <mateusz_gacek@interia.pl>
2024-06-25 19:07:21 +0200

    feat: added mealsItem

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+
 const Meals = () => {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicioys meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your recipe!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
---
+++ app/type/mealItemType.ts
@@ -0,0 +1,8 @@
+export type MealItemType = {
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  creator: string;
+  id?: string;
+};
