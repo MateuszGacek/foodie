cfac2ab3e50142d8772e2ea820f33505946f43e4
MateuszGacek <mateusz_gacek@interia.pl>
2024-07-01 19:41:27 +0200

    feat: catch error when fail fetch meals

--- app/lib/meals.ts
+++ app/lib/meals.ts
@@ -5,6 +5,7 @@ const db = sql("meals.db");
 
 const getMeals: () => MealItemType[] = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
+  // throw new Error("Loading meals failed");
   return db.prepare("SELECT * FROM meals").all() as MealItemType[];
 };
 
